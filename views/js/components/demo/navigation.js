a94f0e4d655e8d3453b459b04ec7dc7d2f1229c0
Khalid Madih <madih_khalid@hotmail.com>
2018-04-15 10:07:44 -0700

    fixed issue with demo login

--- views/js/components/demo/navigation.js
+++ views/js/components/demo/navigation.js
@@ -8,11 +8,11 @@ export default function Navigation(props) {
       <input type="checkbox" id="navbar" checked={props.checked} />
       <label onClick={ props.toggleNavbar } htmlFor="navbar"></label>
       <ul>
-        <li className="nav__brand"><Link onClick={ props.toggleNavbar } to="/app/home">Happen'n</Link></li>
-        <li className="nav__item"><Link onClick={ props.toggleNavbar }  activeStyle={{ color: '#63ccff' }} to="/app/events" >All Events</Link></li>
-        <li className="nav__item"><Link onClick={ props.toggleNavbar }  activeStyle={{ color: '#63ccff' }} to="/app/postevents" >Add Event</Link></li>
+        <li className="nav__brand"><Link onClick={ props.toggleNavbar } to="/demo/home">Happen'n</Link></li>
+        <li className="nav__item"><Link onClick={ props.toggleNavbar }  activeStyle={{ color: '#63ccff' }} to="/demo/events" >All Events</Link></li>
+        <li className="nav__item"><Link onClick={ props.toggleNavbar }  activeStyle={{ color: '#63ccff' }} to="/demo/postevents" >Add Event</Link></li>
 
-        <li className="nav__item"><Link onClick={ props.toggleNavbar } activeStyle={{ color: '#63ccff' }} to="/app/profile">My Events</Link></li>
+        <li className="nav__item"><Link onClick={ props.toggleNavbar } activeStyle={{ color: '#63ccff' }} to="/demo/profile">My Events</Link></li>
                  <li className="nav__item"><Link onClick={ props.toggleNavbar } activeStyle={{ color: '#63ccff' }} to="/">Logout</Link></li>
       </ul>
     </div>
